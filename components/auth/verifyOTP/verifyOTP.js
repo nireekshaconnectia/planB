6252db497176f3b25977f2dcae69cb16de584722
Manish <manishwebworks@gmail.com>
2025-02-02 16:17:38 +0530

    commit

--- components/auth/verifyOTP/verifyOTP.js
+++ components/auth/verifyOTP/verifyOTP.js
@@ -51,7 +51,7 @@ const OTPPopup = ({ onClose, onResend }) => {
             Submit
           </button>
           <div>
-            <label htmlFor="">You haven't received a code? </label>
+            <label htmlFor="">You haven&lsquot received a code? </label>
             <button
               onClick={handleResend} 
               className={styles.otpresend}>
